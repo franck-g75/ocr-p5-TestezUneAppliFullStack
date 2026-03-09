edd62c3d2dc9dd1d93a958a4296c12407f66eaf7
franck-g75 <franc@DELL-FG>
2026-03-09 08:12:45 +0100

    essai de résolution du pbm de proxy

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -13,11 +13,13 @@ export default defineConfig({
   }
 })*/
 
+
 export default defineConfig({
   videosFolder: 'cypress/videos',
   screenshotsFolder: 'cypress/screenshots',
   fixturesFolder: 'cypress/fixtures',
   video: false,
+
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
@@ -25,6 +27,7 @@ export default defineConfig({
       return require('./cypress/plugins/index.ts').default(on, config)
     },
     baseUrl: 'http://localhost:4200',
+    //apiUrl: "http://localhost:8080"
   },
 })
 
